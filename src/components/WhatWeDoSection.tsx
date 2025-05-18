ff1e58e7b7d7a092d7a2bd518ed8134ec297664c
ryanmc1205 <i2yan5@hotmail.com>
2025-05-17 20:48:46 -0500

    Update WhatWeDoSection.tsx

--- src/components/WhatWeDoSection.tsx
+++ src/components/WhatWeDoSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WhatWeDoSection = () => {
   return (
-    <section id="what-we-do" className="section-container bg-white py-20">
+    <section id="what-we-do" className="section-container bg-trasparent py-20">
       <div className="max-w-4xl mx-auto text-center px-4">
         <h2 className="section-title">
           Execution You Can Trust. Events You’ll Remember.
