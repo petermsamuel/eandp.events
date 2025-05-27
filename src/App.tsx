ccaec53d678476dfc2a4a76ed74c4633de43c266
petermsamuel <peter.m.samuel@gmail.com>
2025-05-26 23:11:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import WeddingsPage from "./pages/weddings";
 import CorporatePage from "./pages/corporate";
+import 5QuestionsPage from "./pages/5-questions";
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Route path="/" element={<Index />} />
       <Route path="/weddings" element={<WeddingsPage />} />
       <Route path="/corporate" element={<CorporatePage />} />
+      <Route path="/5-questions" element={<5QuestionsPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
