008545690987a07d91d065bb5837562024d4be9b
petermsamuel <peter.m.samuel@gmail.com>
2025-05-31 13:14:20 -0500

    Update TestimonialSection.tsx

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -85,8 +85,7 @@ const TestimonialSection: React.FC<Partial<Props>> = ({
 
 
 <div
-  className={`max-w-6xl mx-auto bg-white/80 p-6 md:p-10 rounded-lg shadow-md relative flex flex-col ${minHeight}`}
-  style={{ height: "100%", maxHeight: "100%", overflow: "hidden" }}
+  className={`max-w-6xl mx-auto bg-white/80 p-6 md:p-10 rounded-lg shadow-md relative flex flex-col h-[680px] md:h-[540px]`}
 >
         <div className="absolute top-6 left-6 text-6xl text-gold opacity-20 z-0">"</div>
 
