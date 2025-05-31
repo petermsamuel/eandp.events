a3c23c7ca6716da813903a52d51fecbfd37d63f3
petermsamuel <peter.m.samuel@gmail.com>
2025-05-31 15:49:12 -0500

    Update use-analytics.ts

--- src/hooks/use-analytics.ts
+++ src/hooks/use-analytics.ts
@@ -1,7 +1,15 @@
-// src/hooks/useAnalytics.ts
-useEffect(() => {
-  gtag('config', 'G-QMY9ZR38N1', {
-    page_path: location.pathname,
-  });
-}, [location]);
+// src/hooks/use-analytics.ts
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+export function useAnalytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-QMY9ZR38N1", {
+        page_path: location.pathname,
+      });
+    }
+  }, [location.pathname]);
+}
