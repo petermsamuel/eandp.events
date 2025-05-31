1f385c789177c04790972295537cca7440432218
petermsamuel <peter.m.samuel@gmail.com>
2025-05-31 15:44:11 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import NotFound from "./pages/NotFound";
 import WeddingsPage from "./pages/weddings";
 import CorporatePage from "./pages/corporate";
 import FiveQuestionsPage from "./pages/5-questions";
+import { useAnalytics } from "@/hooks/useAnalytics";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/weddings" element={<WeddingsPage />} />
-      <Route path="/corporate" element={<CorporatePage />} />
-      <Route path="/5-questions" element={<FiveQuestionsPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  useAnalytics();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/weddings" element={<WeddingsPage />} />
+        <Route path="/corporate" element={<CorporatePage />} />
+        <Route path="/5-questions" element={<FiveQuestionsPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
