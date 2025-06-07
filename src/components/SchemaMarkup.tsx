e4e9c66989cf9fa77e2bfe1cda2901ae30667023
petermsamuel <peter.m.samuel@gmail.com>
2025-06-07 15:37:23 -0500

    Create SchemaMarkup.tsx

---
+++ src/components/SchemaMarkup.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SchemaMarkup = () => {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "E and P Events",
+    "url": "https://www.eandp.events",
+    "telephone": "+1-770-410-8302",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Atlanta",
+      "addressRegion": "GA",
+      "addressCountry": "US"
+    },
+    "description": "Luxury wedding and corporate event planning in Atlanta, specializing in South Asian and fusion weddings."
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  );
+};
+
+export default SchemaMarkup;
