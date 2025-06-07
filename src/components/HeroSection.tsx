b24bd34e1c56ac8584827989634b7e403bfb310f
petermsamuel <peter.m.samuel@gmail.com>
2025-06-07 14:13:28 -0500

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,13 @@ const HeroSection = () => {
         <h1 className="text-3xl md:text-5xl font-bold mb-6 text-white leading-snug max-w-[75rem] mx-auto">
           When the Stakes Are High, You’ve Got One Shot to Get It Right.
         </h1>
+
+<p className="text-lg md:text-xl mb-6 text-white max-w-3xl mx-auto">
+  Atlanta’s trusted partner for South Asian weddings and corporate event planning.
+</p>
+
       </div>
+
 
       {/* Subhead & Button */}
       <div className="container mx-auto max-w-4xl text-center z-10 animate-fade-in">
