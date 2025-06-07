0888fa9b198e48b16836f8715e394f15c391df3f
petermsamuel <peter.m.samuel@gmail.com>
2025-06-07 15:45:02 -0500

    Update weddings.tsx

--- src/pages/weddings.tsx
+++ src/pages/weddings.tsx
@@ -189,7 +189,7 @@ const Weddings = () => {
   </div>
 
 
-  <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
     <AccordionCard
       icon={Check}
       title="Multiday timelines?"
