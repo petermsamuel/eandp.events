7166badaba25cb4c04be163f336dbad131813dca
petermsamuel <peter.m.samuel@gmail.com>
2025-06-14 15:31:24 -0500

    Create blogData.ts

---
+++ src/blogData.ts
@@ -0,0 +1,7 @@
+export const blogPosts = {
+  "what-does-a-corporate-event-planner-do": {
+    title: "What Does a Corporate Event Planner Actually Do?",
+    component: () => import("./pages/blog/what-does-a-corporate-event-planner-do"),
+  },
+  // Add more posts here...
+};
