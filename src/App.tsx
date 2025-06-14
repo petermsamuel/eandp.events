181046043121c6da1cae30c2b65a85c5e8c3c009
petermsamuel <peter.m.samuel@gmail.com>
2025-06-14 14:49:28 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import WeddingsPage from "./pages/weddings";
 import CorporatePage from "./pages/corporate";
+import BlogPage from "./pages/blog";
 import FiveQuestionsPage from "./pages/5-questions";
 import IGRedirect from "./pages/IGRedirect";
 import { useAnalytics } from "@/hooks/use-analytics";
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/" element={<Index />} />
         <Route path="/weddings" element={<WeddingsPage />} />
         <Route path="/corporate" element={<CorporatePage />} />
+    <Route path="/blog" element={<BlogPage />} />
         <Route path="/5-questions" element={<FiveQuestionsPage />} />
         <Route path="/ig" element={<IGRedirect />} />
         <Route path="*" element={<NotFound />} />
