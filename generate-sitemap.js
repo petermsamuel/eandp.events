e7150c3242c09037a39c2d302f6efaa7a10be18b
petermsamuel <peter.m.samuel@gmail.com>
2025-06-15 13:23:16 -0500

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// 🔧 Required for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const BASE_URL = 'https://www.eandp.events';
 
@@ -12,29 +17,27 @@ const staticRoutes = [
 ];
 
 const blogPostsDir = path.join(__dirname, 'src/pages/blog/');
-const blogSlugs = fs
-  .readdirSync(blogPostsDir)
+const files = await fs.readdir(blogPostsDir);
+
+const blogSlugs = files
   .filter(file => file.endsWith('.tsx'))
   .map(file => `/blog/${file.replace('.tsx', '')}`);
 
 const allRoutes = [...staticRoutes, ...blogSlugs];
 
-const sitemapEntries = allRoutes
-  .map(route => {
-    return `
+const sitemapEntries = allRoutes.map(route => `
   <url>
     <loc>${BASE_URL}${route}</loc>
     <changefreq>monthly</changefreq>
     <priority>${route === '/' ? '1.0' : '0.7'}</priority>
-  </url>`;
-  })
-  .join('');
+  </url>
+`).join('');
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${sitemapEntries}
 </urlset>`;
 
-fs.writeFileSync(path.join(__dirname, 'public/sitemap.xml'), sitemap);
+await fs.writeFile(path.join(__dirname, 'public/sitemap.xml'), sitemap);
 
 console.log('Sitemap generated at public/sitemap.xml');
