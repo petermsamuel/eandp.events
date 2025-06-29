64a99dbf830ca0ab827bd873dde68fc37b811912
petermsamuel <peter.m.samuel@gmail.com>
2025-06-29 12:24:20 -0500

    Create index.page.tsx

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to E&P Events!</h1>
+      <p>This is a placeholder page for SSR setup.</p>
+    </div>
+  );
+}
