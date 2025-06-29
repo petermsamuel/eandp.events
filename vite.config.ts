6cab4d14fc0b4c7451d79f4575a22c7fd1d51588
petermsamuel <peter.m.samuel@gmail.com>
2025-06-29 17:29:27 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import ssr from 'vite-plugin-ssr/plugin';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), ssr({ pageFiles: ['src/pages/**/*.page.*([a-z])'] })],
+  plugins: [
+    react(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
