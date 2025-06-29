24c9f9a82e4934193f32f064ed84e1985767a869
petermsamuel <peter.m.samuel@gmail.com>
2025-06-29 16:39:45 -0500

    Update index.page.tsx

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,5 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Welcome to E&P Events!</h1>
-      <p>Luxury South Asian wedding planning in Atlanta.</p>
-    </main>
-  );
-}
+import React from "react";
+import Index from "../index";
+
+export default Index;
+
