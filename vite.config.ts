bda17b927d664b5a81fb2f3ff6729eea4d83bfe9
petermsamuel <peter.m.samuel@gmail.com>
2025-06-29 13:37:47 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -10,8 +9,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    ssr(), 
-  ],
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
