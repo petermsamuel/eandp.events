f07128def1b1b0c4c9aded842eb653d480209061
petermsamuel <peter.m.samuel@gmail.com>
2025-07-01 22:41:39 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -39,6 +39,34 @@ const Index = () => {
           <AdditionalCtaSection />
           <ContactSection />
         </main>
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "E and P Events",
+      "image": "https://eandp.events/logo.png",
+      "url": "https://eandp.events/",
+      "telephone": "+1-770-410-8302",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "1691 Ashbury Park Dr.",
+        "addressLocality": "Hoschton",
+        "addressRegion": "GA",
+        "postalCode": "30548",
+        "addressCountry": "US"
+      },
+      "priceRange": "$$$",
+      "description": "Atlanta event planners specializing in South Asian weddings, fusion celebrations, and high-end corporate experiences.",
+      "sameAs": [
+        "https://www.instagram.com/eandp_events/",
+        "https://www.facebook.com/eventsep/",
+        "https://www.theknot.com/marketplace/eandp-events-alpharetta-ga-1055902"
+      ]
+    })
+  }}
+/>
         <Footer />
       </div>
     </div>
