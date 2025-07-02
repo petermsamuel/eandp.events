51047ecb0d313416bdff66f1e42c62566f1030d7
petermsamuel <peter.m.samuel@gmail.com>
2025-07-02 17:38:09 -0500

    Update blogData.ts

--- src/blogData.ts
+++ src/blogData.ts
@@ -3,5 +3,9 @@ export const blogPosts = {
     title: "What Does a Corporate Event Planner Actually Do?",
     component: () => import("./pages/blog/what-does-a-corporate-event-planner-do"),
   },
+"hidden-costs-diy-corporate-events": {
+    title: "The Hidden Cost of DIY Corporate Events",
+    component: () => import("./pages/blog/hidden-costs-diy-corporate-events"),
+  },
   // Add more posts here...
 };
