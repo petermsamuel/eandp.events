516e388acd71bdfd70714f5ef214f5221aa27bed
petermsamuel <peter.m.samuel@gmail.com>
2025-07-05 13:47:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 }));
